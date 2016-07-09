36bdc63ec77d18a9ccf630ab14f2f6335cf8d3b4
promizadhel <promiz.adhel@gmail.com>
2016-07-09 15:34:36 +0800

    Gitignore
    added gitignore

---
+++ djnotes/notes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+	title = models.CharField(max_length=512, blank=True)
+	note = models.TextField(blank=True)
+
+	def __str__(self):
+		return self.title
